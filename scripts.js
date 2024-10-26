8173f9c3bcad53bac09ec9d09d9f3e013ce5d441
phumebyte <phumelele.dlamini1000@gmail.com>
2024-10-26 21:40:57 +0200

    Removed throw statements and updated division calculation to display result with two decimal places

--- scripts.js
+++ scripts.js
@@ -18,20 +18,18 @@ form.addEventListener("submit", (event) => {
   if (isNaN(floatDividend) || isNaN(floatDivider)){
     result.classList.add("error-message")
     result.innerText = "Please enter both dividend and divider"
-    throw new Error("Please enter valid numbers");
   } else if (floatDivider === 0){
     // If divider value is 0, answer is undefined
     result.classList.add("error-message")
     result.innerText = "Cannot divide by zero"
-    throw new Error("Cannot divide by zero");
   } else {
     // If inputs are valid, display the answer and remove error message class
     result.classList.remove("error-message")
-    result.innerText = Math.floor(floatDividend / floatDivider);
+    result.innerText = (floatDividend / floatDivider).toFixed(2) // Removed math.floor usage because it rounds down the number
   }
 } catch  (error) {
   // Catch any errors during execution
-  console.error("An error occured:",error.message);
+  console.error("An error occured:",error.message)
   result.classList.add("error-message")
   result.innerText = "An unexpected error occured. Please try again."
 }
